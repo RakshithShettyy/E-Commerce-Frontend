fcfc76e84ccd288e0f52d9f172e35b973524b284
RakshithShettyy <rakshithshetty227@gmail.com>
2024-06-05 11:08:07 -0700

    [ADD] BreadCrumb, Container Component

---
+++ src/components/Container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = ({ children }) => {
+  return <div className="container mx-auto p-4 mt-20">{children}</div>;
+};
+
+export default Container;
--- src/containers/ProductDetails.jsx
+++ src/containers/ProductDetails.jsx
@@ -3,6 +3,22 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { decodeId } from "@/lib/crpyto";
 import { LineWave } from "react-loader-spinner";
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Container from "@/components/Container";
 
 const ProductDetails = () => {
   const { productId } = useParams();
@@ -11,6 +27,7 @@ const ProductDetails = () => {
   const decodedId = decodeId(productId);
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     axios
       .get(`https://dummyjson.com/products/${decodedId}`)
       .then((response) => {
@@ -25,61 +42,86 @@ const ProductDetails = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center">
-        <LineWave
-          visible={true}
-          height="100"
-          width="100"
-          color="#000000"
-          ariaLabel="line-wave-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-        />
-      </div>
+      <Container>
+        <div className="flex items-center justify-center min-h-screen">
+          <LineWave
+            visible={true}
+            height="100"
+            width="100"
+            color="#000000"
+            ariaLabel="line-wave-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      </Container>
     );
   }
 
   return (
-    <div>
-      {product ? (
-        <>
-          <div className="product-details-container">
-            <div className="product-detail-image">
-              <img
-                src={product.thumbnail}
-                alt={`Thumbnail of ${product.title}`}
-              />
-            </div>
-            <div className="product-detail-info">
-              <div>
-                <span className="flex items-center ">
-                  <h2 className="product-detail-title">{product.title}</h2>{" "}
-                  <span className="text-red-600 ml-5 text-xl my-auto">
-                    -{product.discountPercentage}%
-                  </span>
-                </span>
-                <span className="product-detail-links">
-                  {product.shippingInformation} | {product.returnPolicy}
-                </span>
-
-                <p className="product-detail-description">
-                  {product.description}
-                </p>
-                <p className="product-detail-price">Price: ${product.price}</p>
-                <p className="product-detail-discountPercentage text-red-600 text-xl my-auto">
-                  Discount: {product.discountPercentage.toFixed(2)}% OFF
-                </p>
+    <Container>
+      <div className="breadcrumb">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/" className="capitalize">
+                {product.category}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="capitalize">
+                {product.title}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div>
+        {product ? (
+          <>
+            <div className="product-details-container">
+              <div className="product-detail-image">
+                <img
+                  src={product.thumbnail}
+                  alt={`Thumbnail of ${product.title}`}
+                />
               </div>
-              <div></div>
-            </div>
+              <div className="product-detail-info">
+                <div>
+                  <span className="flex items-center ">
+                    <h2 className="product-detail-title">{product.title}</h2>{" "}
+                    <span className="text-red-600 ml-5 text-xl my-auto">
+                      -{product.discountPercentage}%
+                    </span>
+                  </span>
+                  <span className="product-detail-links">
+                    {product.shippingInformation} | {product.returnPolicy}
+                  </span>
 
-            {/* Add more product details as needed */}
-          </div>
-        </>
-      ) : (
-        <p>Product not found</p>
-      )}
-    </div>
+                  <p className="product-detail-description">
+                    {product.description}
+                  </p>
+                  <p className="product-detail-price">
+                    Price: ${product.price}
+                  </p>
+                  <p className="product-detail-discountPercentage text-red-600 text-xl my-auto">
+                    Discount: {product.discountPercentage.toFixed(2)}% OFF
+                  </p>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <p>Product not found</p>
+        )}
+      </div>
+    </Container>
   );
 };
 
